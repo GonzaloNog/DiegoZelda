890002346bee47aefcae61b9e74870b541ba8db7
GonzaloNog <63429016+GonzaloNog@users.noreply.github.com>
2024-04-17 12:44:13 -0300

    Finish

--- Assets/link.cs
+++ Assets/link.cs
@@ -6,11 +6,17 @@ public class link : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    private Animator anim;
+    private AudioSource audioSource;
+    private bool moveWater = false;
+    private bool inWater = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0f; // Desactivar la gravedad
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
     // Update is called once per frame
     void Update()
@@ -21,5 +27,45 @@ public class link : MonoBehaviour
         Vector3 movimiento = new Vector3(moveHorizontal, moveVertical,0f) *moveSpeed *Time.deltaTime;;
 
         transform.Translate(movimiento);
+        anim.SetFloat("vertical", moveVertical);
+        anim.SetFloat("horizontal", moveHorizontal);
+        if(moveVertical != 0 || moveHorizontal != 0)
+        {
+            if (inWater && !audioSource.isPlaying)
+            {
+                moveWater = true;
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            if (inWater)
+            {
+                moveWater = false;
+                audioSource.Pause();
+            }
+            else
+            {
+                moveWater = false;
+                audioSource.Stop();
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Trampa")
+        {
+            inWater = true;
+            Debug.Log("EntroCharco");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Trampa")
+        {
+            inWater = false;
+            Debug.Log("SalioCharco");
+            audioSource.Stop();
+        }
     }
 }
